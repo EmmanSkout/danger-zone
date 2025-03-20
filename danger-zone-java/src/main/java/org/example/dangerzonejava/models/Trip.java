88e2ccac128583eacee117e271095be9aeaaa4d5
PRODYNA\eskoutelis <emmanouil.skoutelis@prodyna.com>
2025-03-20 10:25:36 +0200

    Add trips stuff

--- danger-zone-java/src/main/java/org/example/dangerzonejava/models/Trip.java
+++ danger-zone-java/src/main/java/org/example/dangerzonejava/models/Trip.java
@@ -1,48 +1,46 @@
 package org.example.dangerzonejava.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignores any extra fields in JSON
 public class Trip {
 
-    public Trip(String title,String image,String description){
+    private String title;
+    private String image;
+    private String description;
+    private int dangerScore;
+
+    // No-argument constructor (needed for Jackson)
+    public Trip() {}
+
+    // Constructor with all fields
+    public Trip(String title, String image, String description, int dangerScore) {
         this.title = title;
         this.image = image;
         this.description = description;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getimage() {
-        return image;
-    }
-
-    public void setimage(String image) {
-        this.image = image;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    String title;
-    String image;
-
-    public int getDangerScore() {
-        return dangerScore;
-    }
-
-    public void setDangerScore(int dangerScore) {
         this.dangerScore = dangerScore;
     }
 
-    int dangerScore;
-    String description;
+    // Getters and Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public int getDangerScore() { return dangerScore; }
+    public void setDangerScore(int dangerScore) { this.dangerScore = dangerScore; }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "title='" + title + '\'' +
+                ", image='" + image + '\'' +
+                ", description='" + description + '\'' +
+                ", dangerScore=" + dangerScore +
+                '}';
+    }
 }
--- danger-zone-java/src/main/java/org/example/dangerzonejava/services/TripsService.java
+++ danger-zone-java/src/main/java/org/example/dangerzonejava/services/TripsService.java
@@ -1,14 +1,23 @@
 package org.example.dangerzonejava.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.dangerzonejava.models.Trip;
+import org.springframework.core.io.ClassPathResource;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class TripsService {
 
 
-    public List<Trip> getTrips(){
-        return Collections.emptyList();
+    public List<Trip> loadTripsFromJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (InputStream inputStream = new ClassPathResource("trips.json").getInputStream()) {
+            return objectMapper.readValue(inputStream, new TypeReference<List<Trip>>() {});
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading JSON file", e);
+        }
     }
 }
