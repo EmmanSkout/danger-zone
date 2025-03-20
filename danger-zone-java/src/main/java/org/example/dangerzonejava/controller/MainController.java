67110fa3237c95b941f2da6cb78d72b95ad9d300
Christos Avramis <christos.avramis@prodyna.com>
2025-03-20 09:43:16 +0200

    init controller

---
+++ danger-zone-java/src/main/java/org/example/dangerzonejava/controller/MainController.java
@@ -0,0 +1,17 @@
+package org.example.dangerzonejava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String index(Model model) {
+        return "index";
+    }
+
+
+    public String getTrip()
+}
