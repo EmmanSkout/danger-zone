cc9d5baf9c39825b0bd82624755407b3e619af83
Christos Avramis <christos.avramis@prodyna.com>
2025-03-20 09:39:48 +0200

    init

---
+++ danger-zone-java/src/main/java/org/example/dangerzonejava/DangerZoneJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.dangerzonejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangerZoneJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangerZoneJavaApplication.class, args);
+    }
+
+}
---
+++ danger-zone-java/src/test/java/org/example/dangerzonejava/DangerZoneJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dangerzonejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangerZoneJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
